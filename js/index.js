c549605177eeedc4eb45d1f6b0b3e218f1578ced
Timur Gapterakhmanov(PC) <stenlypaints@gmail.com>
2024-03-12 14:01:16 +0300

    12.03.2024

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const form = document.querySelector('.form');
-const formFieldsets = document.querySelectorAll('.form__fieldset');
 const formBtnPrev = document.querySelector('.form__btn_prev');
 const formBtnNext = document.querySelector('.form__btn_next');
 const formBtnSubmit = document.querySelector('.form__btn_submit');
+const formTime = document.querySelector('.form__time')
+const formFieldsetClient = document.querySelector('.form__fieldset_client')
+const formFieldsetType = document.querySelector('.form__fieldset_type')
+const formFieldsetDate = document.querySelector('.form__fieldset_date')
+const formFieldsets = [formFieldsetType,formFieldsetDate,formFieldsetClient]
 
 let currentStep = 0;
 
@@ -31,21 +35,68 @@ const updateFieldsetVisibility = () => {
     }
 }
 
-formBtnNext.addEventListener('click', () => {
-if (currentStep < formFieldsets.length - 1){
-    currentStep += 1;
-    updateFieldsetVisibility()
+const createFormTime = () => {
+    formTime.style.display = 'block'
 }
-})
 
-formBtnPrev.addEventListener('click', () => {
-    if (currentStep > 0){
-        currentStep -= 1;
-        updateFieldsetVisibility()
+const handleInputForm = ({currentTarget,target}) => { // деструктуризация
+    if(currentTarget.type.value && currentStep === 0){
+        formBtnNext.disabled = false
+
+        // createFormMonth()
+        // createFormDay()
     }
-})
-updateFieldsetVisibility()
 
-formBtnSubmit.addEventListener('click', () => {
+    if(currentStep === 1) {
+        if(currentTarget.day.value && target.name === 'day'){
+
+
+            createFormTime()
+        }
+
+        if(currentTarget.day.value && currentTarget.time.value && target.name === 'time'){
+            formBtnNext.disabled = false
+        } else {
+            formBtnNext.disabled = true
+        }
+    }
+
+    if(currentStep === 2) {
+        const inputs = formFieldsetClient.querySelectorAll('.form__input')
+        let allFilled = true 
+
+        inputs.forEach(input => {
+            if (input.value.trim() === ''){
+                allFilled = false
+            }
+        });
+
+        formBtnSubmit.disabled = !allFilled
+    }
+}
+
+
+const init = () => {
+    formBtnNext.disabled = true
+    formBtnNext.addEventListener('click', () => {
+        if (currentStep < formFieldsets.length - 1){
+            currentStep += 1;
+            updateFieldsetVisibility()
+            formBtnNext.disabled = true
+            formBtnSubmit.disabled = true
+        }
+        })
+        
+    formBtnPrev.addEventListener('click', () => {
+        if (currentStep > 0){
+            currentStep -= 1;
+            updateFieldsetVisibility()
+            formBtnNext.disabled = false
+        }
+    })
+    updateFieldsetVisibility()
     
-})
+    form.addEventListener('input', handleInputForm)
+}
+
+init()
